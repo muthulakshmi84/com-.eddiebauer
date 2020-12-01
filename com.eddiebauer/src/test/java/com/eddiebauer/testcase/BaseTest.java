b13176c3815f43cf8bc85941680e64b314ca2929
MuthuMalai <MuthuMalai@LAPTOP-UR72A20M>
2020-12-01 13:31:04 -0500

    Committing changes with Automation Report

--- com.eddiebauer/src/test/java/com/eddiebauer/testcase/BaseTest.java
+++ com.eddiebauer/src/test/java/com/eddiebauer/testcase/BaseTest.java
@@ -1,14 +1,28 @@
 package com.eddiebauer.testcase;
 
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.eddiebauer.base.DriverScript;
 import com.eddiebauer.pages.HomePage;
 
 public class BaseTest extends DriverScript{
 	
 	HomePage homePage;
+	ExtentReports extent;
+	ExtentTest logger;
+	
+	@BeforeClass
+	public void preTest(){
+		ExtentHtmlReporter htmlReport=new ExtentHtmlReporter("./report/AutomationReport.html");
+		extent=new ExtentReports();
+		extent.attachReporter(htmlReport);
+	}
 	
 	@BeforeTest
 	public void preCondition(){
@@ -24,4 +38,8 @@ public class BaseTest extends DriverScript{
 		//closeApplication();
 	}
 	
+	@AfterClass
+	public void tearDown(){
+		extent.flush();
+	}
 }
--- com.eddiebauer/src/test/java/com/eddiebauer/testcase/TC01_ClickHomePage.java
+++ com.eddiebauer/src/test/java/com/eddiebauer/testcase/TC01_ClickHomePage.java
@@ -2,17 +2,27 @@ package com.eddiebauer.testcase;
 
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.Status;
+
 public class TC01_ClickHomePage extends BaseTest{
 	
 	@Test
 	public void testClickViewAllOffers(){
+		logger=extent.createTest("Validate clicking view all offers link");
 		homePage.clickViewAllOffersLink();
+		logger.log(Status.PASS, "Clicked on View All Offers Link");
 		homePage.clickDetailsButton1();
+		logger.log(Status.PASS, "Clicked on Details Button1");
 		homePage.clickCloseDetailsButton1();
+		logger.log(Status.PASS, "Clicked on Close Details Button1");
 		homePage.clickDetailsButton2();
+		logger.log(Status.PASS, "Clicked on Details Button2");
 		homePage.clickCloseDetailsButton2();
+		logger.log(Status.PASS, "Clicked on Close Details Button2");
 		homePage.clickDetailsButton3();
+		logger.log(Status.PASS, "Clicked on Details Button3");
 		homePage.clickCloseDetailsButton3();
+		logger.log(Status.PASS, "Clicked on Close Details Button3");
 		homePage.clickRightArrowButton();
 	}
 }
